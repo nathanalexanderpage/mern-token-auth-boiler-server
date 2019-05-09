5094c0d3e0de2189b99c3be486d26e4d525638e7
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-05-09 11:21:11 -0700

    added password-excluding functionality to user model

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+let bcrypt = require('bcryptjs');
 var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
@@ -23,13 +24,28 @@ var userSchema = new mongoose.Schema({
 });
 
 // TODO: Override 'toJSON' to prevent the password from being returned with the user
+userSchema.set('toJSON', {
+  transform: (doc, user) => {
+    // delete user.password; // --> alternate
+    // return user;
+    return {
+      id: user._id,
+      name: user.name,
+      email: user.email
+    }
+  }
+});
 
+// A helper function to authenticate with bcrypt
+userSchema.methods.authenticated = function(password) {
+  return bcrypt.compareSync(password, this.password);
+}
 
-// TODO: A helper function to authenticate with bcrypt
-
-
-// TODO: Find out Mongoose's version of a beforeCreate hook
-
+// Find out Mongoose's version of a beforeCreate hook
+userSchema.pre('save', function(next) {
+  this.password = bcrypt.hashSync(this.password, 12);
+  next();
+});
 
 // Exporting the User model
 module.exports = mongoose.model('User', userSchema);
